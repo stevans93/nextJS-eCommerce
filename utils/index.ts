024a0da51a56416851fee27bcbcb52c8e12441b9
stevans93 <stevans93@gmail.com>
2024-02-17 17:17:14 +0100

    end of project

--- utils/index.ts
+++ utils/index.ts
@@ -22,4 +22,4 @@ export const updateSearchParams = (type: string, value: string) => {
     const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
   
     return newPathname;
-  };
+};
